119bc6882a9aa4598ba3687e773e05acb942bb1c
DrFancy <thomasjordane7@gmail.com>
2016-07-14 22:28:07 -0400

    The place where the play happens
    This will be were are everything is activated.

---
+++ src/Main_Play_Place.java
@@ -0,0 +1,13 @@
+/* The Stage is the place were the player and the games meet for an epic play
+ * the player is created hear, and the game ask for their information.
+ * the the game is chosen, from a menu of choices and then generated and the for the player to play.
+ * 
+ */
+public class Main_Play_Place {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
